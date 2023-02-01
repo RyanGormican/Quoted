a1c8363ea674242bb2cb47e3f4f55d40bc15838b
RyanGormican <ryan.gormican@gmail.clom>
2023-01-31 21:47:20 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -30,15 +30,17 @@ class App extends React.Component {
 				<div className="card">
 					<h1 className ="heading">{quote}</h1>
 					<button className="button" onClick={this.grabQuote}>
-						<span>Generate Quote</span>
-					</button>
-					<div>
-						<a href="https://www.linkedin.com/in/ryangormican/">
-							<Icon icon="mdi:linkedin" color="#0e76a8" width="40" />
-						</a>
 						<a href="https://github.com/RyanGormican/Quoted">
 							<Icon icon="mdi:github" color="white" width="40" />
 						</a>
+						<span>Generate Quote</span>
+						<a href="https://github.com/RyanGormican/Quoted">
+							<Icon icon="mdi:github" color="white" width="40" />
+						</a>
+					</button>
+					<div>
+						
+						
 					</div>
 				</div>
 			</div>
