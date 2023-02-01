a9408563e49da13619f480078dcb1caba779a02b
RyanGormican <ryan.gormican@gmail.clom>
2023-01-31 21:01:37 -0600

    a

--- src/App.js
+++ src/App.js
@@ -28,6 +28,9 @@ class App extends React.Component {
 			<div className ="app">
 				<div className="card">
 					<h1 className ="heading">{quote}</h1>
+					<button className="button">
+						<span>Generate Quote</span>
+					</button>
 				</div>
 			</div>
 		 );
