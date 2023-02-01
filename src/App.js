6e0533ea7e677abfc0688ce538825f1d32e88fb7
RyanGormican <ryan.gormican@gmail.clom>
2023-01-31 20:29:49 -0600

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends React.Component {
+	state = { quote: '' };
+
+	componentDidMount() {
+		console.log('Test');
+	}
 	render() {
 		 return (
 			<h1> Hello </h1>
